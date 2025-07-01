bb367e601887e528afb3bda68c7244d7f3301220
Cesar <cesarhenrique_prado@hotmail.com>
2025-07-01 16:51:58 -0300

    feat: Implemented Swagger, organization endpoints and implemented Requests(DTOs)

---
+++ ScreenSound.API/Endpoints/MusicasExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Requests;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.API.Endpoints;
+
+public static class MusicasExtensions
+{
+    public static void AddEndpointMusicas(this WebApplication app)
+    {
+        app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
+        {
+            return dal.Listar();
+        });
+
+        app.MapGet("/Musicas/{nome}", ([FromServices] DAL<Musica> dal, string nome) =>
+        {
+            return dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+        });
+
+        app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] MusicaRequest musicaRequest) =>
+        {
+            var musica = new Musica(musicaRequest.nome)
+            {
+                ArtistaId = musicaRequest.ArtistaId,
+                AnoLancamento = musicaRequest.anoLancamento
+            };
+            dal.Adicionar(musica);
+            return Results.Ok();
+        });
+
+        app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) => {
+            var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
+            if (musicaAAtualizar is null)
+            {
+                return Results.NotFound();
+            }
+            musicaAAtualizar.Nome = musica.Nome;
+            musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
+            musicaAAtualizar.Artista = musica.Artista;
+            dal.Atualizar(musicaAAtualizar);
+            return Results.Ok();
+        });
+
+        app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) =>
+        {
+            var musica = dal.RecuperarPor(a => a.Id == id);
+            if (musica is null)
+            {
+                return Results.NotFound();
+            }
+            dal.Deletar(musica);
+            return Results.NoContent();
+        });
+    }
+}
