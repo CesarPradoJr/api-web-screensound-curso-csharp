95263b86cf5e71cd357ef6ce3dcee0f0e7444f24
Cesar <cesarhenrique_prado@hotmail.com>
2025-07-08 16:49:33 -0300

    feat add new endpoints Genero

---
+++ ScreenSound.API/Endpoints/GeneroExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ScreenSound.API.Requests;
+using ScreenSound.API.Response;
+using ScreenSound.Banco;
+using ScreenSound.Shared.Modelos.Modelos;
+
+namespace ScreenSound.API.Endpoints;
+
+public static class GeneroExtensions
+{
+    public static void AddEndpointGeneros(this WebApplication app)
+    {
+        app.MapGet("/Generos", ([FromServices] DAL<Genero> dal) =>
+        {
+            return EntityListToResponseList(dal.Listar());
+        });
+
+        app.MapGet("/Generos/{nome}", ([FromServices] DAL<Genero> dal, string nome) =>
+        {
+            var genero = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+            if (genero != null)
+            {
+                var response = EntityToResponse(genero!);
+                return Results.Ok(response);
+            }
+            return Results.NotFound("Gênero não encontrado.");
+        });
+
+        app.MapPost("/Generos", ([FromServices] DAL<Genero> dal, [FromBody] GeneroRequest generoRequest) =>
+        {
+            dal.Adicionar(RequestToEntity(generoRequest));
+        });
+
+        app.MapDelete("/Generos/{id}", ([FromServices] DAL<Genero> dal, int id) =>
+        {
+            var generoDelete = dal.RecuperarPor(a => a.Id == id);
+            if (generoDelete != null)
+            {
+                dal.Deletar(generoDelete);
+                return Results.NoContent();
+            }
+            return Results.NotFound("Gênero a ser deletado não encontrado");
+        });
+    }
+
+    private static ICollection<GeneroResponse> EntityListToResponseList(IEnumerable<Genero> generos)
+    {
+        return generos.Select(a => EntityToResponse(a)).ToList();
+    }
+
+    private static GeneroResponse EntityToResponse(Genero genero)
+    {
+        return new GeneroResponse(genero.Id, genero.Nome!, genero.Descricao!);
+    }
+
+    private static Genero RequestToEntity(GeneroRequest generoRequest)
+    {
+        return new Genero() { Nome = generoRequest.Nome, Descricao = generoRequest.Descricao };
+    }
+}
---
+++ ScreenSound.API/Response/MusicaResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace ScreenSound.API.Response;
+
+public class MusicaResponse(int Id, string Nome, int ArtistaId);
